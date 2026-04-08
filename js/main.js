78618b22241b1752cc3f4a47c4182414de4cd25e
xc8pt <brolike636@gmail.com>
2026-04-08 09:55:53 +0300

    spread/rest in JS

---
+++ js/main.js
@@ -0,0 +1,93 @@
+console.log("\n=== Деструктиризация массивов ===");
+const colors = ["red", "green", "blue"];
+
+const color1 = colors[0];
+const color2 = colors[1];
+console.log(color1, color2);
+
+const [firstColor, secondColor, thirdColor] = colors;
+console.log(firstColor, secondColor, thirdColor);
+
+const [primary, , tertiary] = colors;
+console.log(primary, tertiary);
+
+const [c1, c2, c3, c4 = "yellow"] = colors;
+console.log(c4);
+
+// console.log("Деструктиризация  объектов");
+const user = {
+  name: "Алиса",
+  age: 25,
+  city: "Москва",
+};
+
+// const userName = user.name;
+// const userAge = user.age;
+// console.log(userName, userAge);
+
+// const { name, age, city } = user;
+// console.log(name,age,city);
+
+// const{name: fullName, age: years} = user;
+// console.log(fullName, years);
+
+// const {name: personName, country = "Россия" } = user;
+// console.log(personName, country);
+
+// console.log("Дестуктиризация в параметрах");
+
+// function printUserOld(user) {
+//     console.log(`Имя: ${user.name}`);
+//     console.log(`Возраст: ${user.age}`);
+//     console.log(`Город: ${user.city}`);
+// }
+
+// function printUser({ name, age, city }){
+//     console.log(`Имя: ${name}`);
+//     console.log(`Возраст: ${age}`);
+//     console.log(`Город: ${city}`);
+// }
+
+// printUserOld(user);
+// console.log();
+// printUser(user);
+
+//console.log("Spread для массивов");
+//const arr1 = [1, 2, 3];
+//const arr2 = [4, 5, 6];
+//const combined = [...arr1, ...arr2];
+//console.log("Объединённый массив:", combined);
+//const copy = [...arr1];
+//console.log("Копия массива:", copy);
+//const extended = [0, ...arr1, 7, 8];
+//console.log("Расширенный массив:", extended);
+
+/*
+console.log("Spread для объектов");
+const person = {
+    name: "Иван",
+    age: 30,
+};
+const address = {
+    city: "Санкт-Петербург",
+    street: "Невский проспект",
+};
+const fullInfo = {...person, ...address};
+console.log("Полная информация:", fullInfo);
+const personCopy = {...person};
+console.log("Копия объекта:", personCopy);
+const updated = {...person, age: 31, occupation:"Developer"};
+console.log("Обновлённый объект:", updated);
+/*/
+
+console.log("Rest оператор");
+function sum(...numbers) {
+  return numbers.reduce((total, num) => total + num, 0);
+}
+console.log("Сумма 1,2,3:", sum(1, 2, 3));
+console.log("Сумма 1,2,3,4,5:", sum(1, 2, 3, 4, 5));
+const numbers = [10, 20, 30, 40, 50];
+const [first, second, ...rest] = numbers;
+console.log("Первое число:", first);
+console.log("Первое число:", second);
+console.log("Первое число:", rest);
